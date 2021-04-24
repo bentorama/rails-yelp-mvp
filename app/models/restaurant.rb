61b3b75c08e87d18d254b2c726d130321ab86412
Edward Bent <bentorama@gmail.com>
2021-04-24 16:54:57 +0100

    Updated with CATEGORIES constant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,7 @@
 class Restaurant < ApplicationRecord
+  CATEGORIES = %w[chinese italian japanese french belgian]
   has_many :reviews, dependent: :destroy
-  validates :category, inclusion: { in: %w[chinese italian japanese french belgian],
+  validates :category, inclusion: { in: CATEGORIES,
                                     message: '%{value} is not a valid category' }
   validates :address, :name, :category, presence: true
 end
