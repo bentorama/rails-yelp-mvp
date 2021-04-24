224cd50f151c031a0c04159c1712bf68c7c6fa2e
Edward Bent <bentorama@gmail.com>
2021-04-24 14:54:37 +0100

    Finished add review and restaurant actions

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,31 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    # TODO
+    @restaurant = Restaurant.new # instantiate the form
+  end
+
+  def create
+    # TODO
+    @restaurant = Restaurant.new(restaurant_params)
+    # associate the restaurant with the corresponding restaurant
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,26 @@
 class ReviewsController < ApplicationController
+  def new
+    # TODO
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new # instantiate the form
+  end
+
+  def create
+    # TODO
+    @review = Review.new(review_params)
+    # associate the review with the corresponding restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
